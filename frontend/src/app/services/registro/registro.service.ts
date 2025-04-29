9c5f997495888ef22bf815b543c5ddbf04e2f4e2
MarcoV <marcovirinni@gmail.com>
2025-04-29 15:20:29 -0300

    refactor : [https://github.com/Web-Developers2-0/2025-Practica_Profesionalizante/issues/50], updated API calls

--- frontend/src/app/services/registro/registro.service.ts
+++ frontend/src/app/services/registro/registro.service.ts
@@ -1,27 +1,30 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class RegistroService {
-
-  constructor(private http: HttpClient) { }
-
-  registerUser(user: any): Observable<any> {
-    return this.http.post<any>('http://127.0.0.1:8000/api/register/', user).pipe(
-      catchError(this.handleError)
-    )
-  }
-
-
-  private handleError(error:HttpErrorResponse){
-    if(error.status === 0){
-      console.error('Se ha producido un error', error.error);
-    }else{
-      console.error(`Backend retorno el código de estado: `, error.status, error.error);
+  import { Injectable } from '@angular/core';
+  import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+  import { Observable, catchError, throwError } from 'rxjs';
+  import { environment } from '../../../environments/environment';
+  
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class RegistroService {
+  
+    private baseUrl = environment.apiUrl;
+  
+    constructor(private http: HttpClient) { }
+  
+    registerUser(user: any): Observable<any> {
+      return this.http.post<any>(`${this.baseUrl}register/`, user).pipe(
+        catchError(this.handleError)
+      );
     }
-    return throwError(() => new Error('Algo salio mal, intente nuevamente'));
+  
+    private handleError(error: HttpErrorResponse) {
+      if (error.status === 0) {
+        console.error('Se ha producido un error:', error.error);
+      } else {
+        console.error(`Backend retornó el código de estado ${error.status}:`, error.error);
+      }
+      return throwError(() => new Error('Algo salió mal, intente nuevamente'));
+    }
   }
-}
+  
