41caaf950b064e32fbcc8f2d4ca3e87df801d8c4
Valentino399 <lorentivalentino@gmail.com>
2025-05-07 21:52:05 -0300

    Rename registro folder to register

---
+++ frontend/src/app/services/register/register.service.ts
@@ -0,0 +1,30 @@
+  import { Injectable } from '@angular/core';
+  import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+  import { Observable, catchError, throwError } from 'rxjs';
+  import { environment } from '../../../environments/environment';
+  
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class RegistroService {
+  
+    private baseUrl = environment.apiUrl;
+  
+    constructor(private http: HttpClient) { }
+  
+    registerUser(user: any): Observable<any> {
+      return this.http.post<any>(`${this.baseUrl}register/`, user).pipe(
+        catchError(this.handleError)
+      );
+    }
+  
+    private handleError(error: HttpErrorResponse) {
+      if (error.status === 0) {
+        console.error('Se ha producido un error:', error.error);
+      } else {
+        console.error(`Backend retornó el código de estado ${error.status}:`, error.error);
+      }
+      return throwError(() => new Error('Algo salió mal, intente nuevamente'));
+    }
+  }
+  
