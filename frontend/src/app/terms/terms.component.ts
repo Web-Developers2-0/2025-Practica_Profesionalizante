44ad682c9745be0f0335073a53d4e3c7080c01e9
Valentino399 <lorentivalentino@gmail.com>
2025-05-09 02:45:54 -0300

    Feature close and accept functionality in terms component ts

--- frontend/src/app/terms/terms.component.ts
+++ frontend/src/app/terms/terms.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-terms',
   standalone: true,
   imports: [],
   templateUrl: './terms.component.html',
-  styleUrl: './terms.component.css'
+  styleUrls: ['./terms.component.css']
 })
 export class TermsComponent {
+  constructor(public dialogRef: MatDialogRef<TermsComponent>) {}
 
+  closeDialog(): void {
+    this.dialogRef.close(false); 
+  }
+
+  acceptTerms(): void {
+    this.dialogRef.close(true); 
+  }
 }
