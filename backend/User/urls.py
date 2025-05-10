1e4b76499540493e10197e6f8c07335b117483ea
Erick <105466809+erick-lpz@users.noreply.github.com>
2025-05-10 02:58:39 -0300

    Feat: [https://github.com/Web-Developers2-0/2025-Practica_Profesionalizante/issues/72] password reset and password reset confirmation endpoints in urls.py

--- backend/User/urls.py
+++ backend/User/urls.py
@@ -14,11 +14,9 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.Login.as_view(), name='login'), 
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    
     path('', include(router.urls)),
-    
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    
     path('user/', views.UserView.as_view(), name='user'),
-   
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
+    path('password-reset-confirm/<str:token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
