f4fa06893990fa1c99c2fedd6967b1215df7a90f
MarcoV <marcovirinni@gmail.com>
2025-05-13 10:11:41 -0300

    chore :[https://github.com/Web-Developers2-0/2025-Practica_Profesionalizante/issues/146] , add vercel.json, and update angular ts config and package.json

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -8,114 +8,64 @@ import { ContactComponent } from '../contact/contact.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-// export class HomeComponent {
-//   counter: number = 0;
-//   operacion: number = 0;
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//     const btnLeft = document.querySelector(".btn-left") as HTMLElement;
-//     const btnRight = document.querySelector(".btn-right") as HTMLElement;
-//     const slider = document.querySelector("#slider") as HTMLElement;
-//     const sliderSections = document.querySelectorAll(".slider-section");
-
-//     btnLeft.addEventListener("click", () => this.moveToLeft());
-//     btnRight.addEventListener("click", () => this.moveToRight());
-
-//     setInterval(() => {
-//         this.moveToRight();
-//     }, 3000);
-//   }
-
-//   private moveToLeft(): void {
-//     const slider = document.querySelector("#slider") as HTMLElement;
-//     const sliderSections = document.querySelectorAll(".slider-section");
-//     const widthImg = 100 / sliderSections.length;
-
-//     this.counter--;
-//     if (this.counter < 0) {
-//         this.counter = sliderSections.length - 1;
-//         this.operacion = widthImg * (sliderSections.length - 1);
-//         slider.style.transform = `translate(-${this.operacion}%)`;
-//         slider.style.transition = "none";
-//         return;
-//     }
-//     this.operacion = this.operacion - widthImg;
-//     slider.style.transform = `translate(-${this.operacion}%)`;
-//     slider.style.transition = "all ease .6s";
-//   }
-
-//   private moveToRight(): void {
-//     const slider = document.querySelector("#slider") as HTMLElement;
-//     const sliderSections = document.querySelectorAll(".slider-section");
-//     const widthImg = 100 / sliderSections.length;
-
-//     if (this.counter >= sliderSections.length - 1) {
-//         this.counter = 0;
-//         this.operacion = 0;
-//         slider.style.transform = `translate(-${this.operacion}%)`;
-//         slider.style.transition = "none";
-//         return;
-//     }
-//     this.counter++;
-//     this.operacion = this.operacion + widthImg;
-//     slider.style.transform = `translate(-${this.operacion}%)`;
-//     slider.style.transition = "all ease .6s";
-//   }
-// }
 
 export class HomeComponent implements OnInit, OnDestroy {
-  counter: number = 0;
-  operacion: number = 0;
-  interval: any;
-  sliderSections: HTMLElement[] = [];
+  
+   counter: number = 0;
+   operacion: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.interval = setInterval(() => {
-      this.moveToRight();
-    }, 3000);
-  }
-
+   constructor() { }
   ngOnDestroy(): void {
-    clearInterval(this.interval);
+    throw new Error('Method not implemented.');
   }
 
-  moveToLeft(): void {
-    this.counter--;
-    if (this.counter < 0) {
-      this.counter = this.sliderSections.length - 1;
-      this.operacion = this.getSectionWidth() * (this.sliderSections.length - 1);
-    } else {
-      this.operacion -= this.getSectionWidth();
-    }
-    this.updateSlider();
-  } 
+   ngOnInit(): void {
+     const btnLeft = document.querySelector(".btn-left") as HTMLElement;
+     const btnRight = document.querySelector(".btn-right") as HTMLElement;
+     const slider = document.querySelector("#slider") as HTMLElement;
+     const sliderSections = document.querySelectorAll(".slider-section");
 
-  moveToRight(): void {
-    if (this.counter >= this.sliderSections.length - 1) {
-      this.counter = 0;
-      this.operacion = 0;
-    } else {
-      this.counter++;
-      this.operacion += this.getSectionWidth();
-    }
-    this.updateSlider();
+     btnLeft.addEventListener("click", () => this.moveToLeft());
+     btnRight.addEventListener("click", () => this.moveToRight());
+
+     setInterval(() => {
+         this.moveToRight();
+     }, 3000);
   }
 
-  updateSlider(): void {
-    const slider = document.querySelector('#slider') as HTMLElement;
-    slider.style.transform = `translate(-${this.operacion}%)`;
-    slider.style.transition = 'all ease .6s';
-  }
+   private moveToLeft(): void {
+     const slider = document.querySelector("#slider") as HTMLElement;
+     const sliderSections = document.querySelectorAll(".slider-section");
+     const widthImg = 100 / sliderSections.length;
 
-  getTotalSections(): number {
-    return this.sliderSections.length;
-  }
+     this.counter--;
+     if (this.counter < 0) {
+         this.counter = sliderSections.length - 1;
+         this.operacion = widthImg * (sliderSections.length - 1);
+         slider.style.transform = `translate(-${this.operacion}%)`;
+         slider.style.transition = "none";
+         return;
+     }
+     this.operacion = this.operacion - widthImg;
+     slider.style.transform = `translate(-${this.operacion}%)`;
+     slider.style.transition = "all ease .6s";
+   }
 
-  getSectionWidth(): number {
-    return 100 / this.getTotalSections();
-  }
-}
+   private moveToRight(): void {
+     const slider = document.querySelector("#slider") as HTMLElement;
+     const sliderSections = document.querySelectorAll(".slider-section");
+     const widthImg = 100 / sliderSections.length;
+
+     if (this.counter >= sliderSections.length - 1) {
+         this.counter = 0;
+         this.operacion = 0;
+         slider.style.transform = `translate(-${this.operacion}%)`;
+         slider.style.transition = "none";
+         return;
+     }
+     this.counter++;
+     this.operacion = this.operacion + widthImg;
+     slider.style.transform = `translate(-${this.operacion}%)`;
+     slider.style.transition = "all ease .6s";
+   }
+ }
